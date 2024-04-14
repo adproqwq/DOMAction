411b0eb1b453f7422c91b45cdd7329e8423cbfba
Adpro <101449500+adproqwq@users.noreply.github.com>
2024-04-14 11:56:59 +0800

    fix: module import error

--- scripts/generateSubFile.ts
+++ scripts/generateSubFile.ts
@@ -2,6 +2,8 @@ import sub from '../src/sub';
 import json5 from 'json5';
 import fs from 'node:fs/promises';
 
-export const generateSubFile = async () => {
-  await fs.writeFile(`${process.cwd}/sub/sub.json5`, json5.stringify(sub));
+const generateSubFile = async () => {
+  await fs.writeFile(`${process.cwd()}/sub/sub.json5`, json5.stringify(sub));
 };
+
+generateSubFile();
